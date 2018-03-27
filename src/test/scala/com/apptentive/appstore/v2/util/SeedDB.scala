52b4466cb6816549c2650ef9d91a8e13d5366d3c
ktsykulenko <konstantin@apptentive.com>
2018-03-27 21:38:09 +0300

    Seed changes to drop tables first.

--- src/test/scala/com/apptentive/appstore/v2/util/SeedDB.scala
+++ src/test/scala/com/apptentive/appstore/v2/util/SeedDB.scala
@@ -2,11 +2,19 @@ package com.apptentive.appstore.v2.util
 
 import com.apptentive.appstore.v2.config.CassandraConfig
 
+import scala.util.Try
+
 object SeedDB extends App {
   val cluster =
     CassandraConfig.defaultCluster
 
   implicit val session = cluster.connect()
 
+  Try(session.execute("DROP KEYSPACE " + CassandraConfig.keyspace))
+
   KeyspaceInitialzier.setupKeyspace()
+
+  session.close()
+
+  cluster.close()
 }
